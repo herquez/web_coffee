546dcf508ec15bb93408cbbf4bdddf07b7ae137c
herquez <miguelherquez@gmail.com>
2023-06-08 19:36:34 -0600

    Load services from the data base and make them dynamic

--- services/views.py
+++ services/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Service as Service_mdl
 
 def services(request):
-    return render(request, 'services/services.html')
+    services_all = Service_mdl.objects.filter(deleted__isnull=True).order_by('created')
+    context = {
+        'services_all': services_all,
+    }
+    return render(request, 'services/services.html', context)
