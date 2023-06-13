7eb4f18c5696ec6b2c29a3dc2d00eaf004d4d494
herquez <miguelherquez@gmail.com>
2023-06-12 18:53:07 -0600

    Add custom to the control panel for service application

--- services/admin.py
+++ services/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Service
 
 class ServiceAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated', 'deleted')
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'subtitle', 'content', 'image', 'created', 'updated')
+    ordering = ('created',)
+    search_fields = ('title', 'subtitle')
+    date_hierarchy = ('created')
 
 admin.site.register(Service, ServiceAdmin)
