da9759ec412819eab4dfc257aec33275359bcb86
herquez <miguelherquez@gmail.com>
2023-06-12 20:15:52 -0600

    Make the blog page content dynamic acording to posts in database

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post as Post_mdl
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post_mdl.objects.filter(deleted__isnull = True)
+    context = {
+        'posts_all': posts
+    }
+    return render(request, 'blog/blog.html', context)
