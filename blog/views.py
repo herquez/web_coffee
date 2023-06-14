59ea5b1636f878a8bf277eb7e3c8f3ff16c2f157
herquez <miguelherquez@gmail.com>
2023-06-13 23:15:34 -0600

    Add category view to make page with the filter of category. Add library to safe requests. Use a parameter to get the category.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post as Post_mdl
+from .models import Category as Category_mld
 
 def blog(request):
     posts = Post_mdl.objects.filter(deleted__isnull = True)
@@ -7,3 +8,10 @@ def blog(request):
         'posts_all': posts
     }
     return render(request, 'blog/blog.html', context)
+
+def category(request, category_id):
+    category = get_object_or_404(Category_mld, id=category_id, deleted__isnull=True)
+    context = {
+        'category': category,
+    }
+    return render(request, 'blog/category.html', context)
