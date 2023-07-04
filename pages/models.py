42247944dc922e7d75e52d726dc4a08311f91bce
herquez <miguelherquez@gmail.com>
2023-07-03 19:45:34 -0600

    Create page model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    title = models.CharField(verbose_name='Titulo', max_length=200)
+    content = models.TextField(verbose_name='Contenido')
+    created = models.DateTimeField(verbose_name='Fecha de creación', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Fecha de edición', auto_now=True)
+    deleted = models.DateTimeField(verbose_name='Fecha de eliminación', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Página'
+        verbose_name_plural = 'Páginas'
+        ordering = ['Title']
+
+    def __str__(self):
+        return self.title
+    
