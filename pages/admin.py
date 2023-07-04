53c34210adc0856f12ff64b842b204a2fc51b11a
herquez <miguelherquez@gmail.com>
2023-07-03 19:47:58 -0600

    Add admin configurations

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated', 'deleted')
+    list_display = ('title', 'content', 'created', 'updated')
+    ordering = ('created',)
+    search_fields = ('title', 'content')
+    date_hierarchy = ('created')
+
+admin.site.register(Page, PageAdmin)
