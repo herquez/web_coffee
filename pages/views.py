73365ed7f625f8dee913843c995f239fa68465db
herquez <miguelherquez@gmail.com>
2023-07-03 20:06:26 -0600

    Create view and url for pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Page as Page_mdl
 
-# Create your views here.
+def page(request, page_id):
+    page = get_object_or_404(Page_mdl, id=page_id, deleted__isnull=True)
+    context = {
+        'page': page,
+    }
+    return render(request, 'pages/sample.html', context)
